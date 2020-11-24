540421ac302a267e1da167e6fb5f44054096a332
ADMINMJ <mjpardogarcia@gmail.com>
2020-11-24 12:12:44 +0100

    segunda version

---
+++ src/cvs/Alumno.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cvs;
+
+/**
+ *
+ * @author ADMINMJ
+ */
+public class Alumno {
+    String nombre;
+    int edad;
+    
+}
